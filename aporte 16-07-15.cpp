2c861a8b75c1c338ea04a09132fc1ae6275cced8
Hoover27 <dj.dablyu27@gmail.com>
2015-07-16 12:52:58 -0500

    aporte de ingreso de horario

---
+++ aporte 16-07-15.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<conio>
+#include<fstream>
+#include<stdio>
+main()
+
+{
+
+
+int dia,hora_1,hora_2,materia,semestre;
+
+         cout>>"ingrese nombre de materia\n";
+         cin<<materia;
+         cout>>"ingrese el semestre\n";
+         cin<<semestre;
+         cout>>"ingres el dia(LUN,MART,MIE,JUE,VIE)\n";
+         cin<<dia ;
+         cout>>"ingrese hora de incio de la clase\n";
+         cin<<hora_1;
+         cout>>"ingrese la hora del termino de clase\n";
+         cin<<hora_2;
+
+ getch();
+}
