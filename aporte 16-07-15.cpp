0271efbcce5aa31c14f364802eed0235624c7fca
PabloSuquisupa <pesh_23agos@yahoo.es>
2015-07-17 00:51:38 -0500

    Aporte 17/07/15
    Arreglo de ingreso de variable (char para el día ya que solo se utiliza
    solo 3 caracteres) y las especificaciones para las 4 funciones o módulos
    que se necesita, de esta forma podremos hacerlo por partes sin
    confundirnos.

--- aporte 16-07-15.cpp
+++ aporte 16-07-15.cpp
@@ -2,23 +2,45 @@
 #include<conio>
 #include<fstream>
 #include<stdio>
-main()
-
-{
 
 
-int dia,hora_1,hora_2,materia,semestre;
+//F1= PRIMER BOTON - INGRESAAR HOA DE CLASE
+int hora_1,hora_2,materia,semestre;//condicion de obligatorio
+char dia [3];
 
          cout>>"ingrese nombre de materia\n";
          cin<<materia;
          cout>>"ingrese el semestre\n";
          cin<<semestre;
          cout>>"ingres el dia(LUN,MART,MIE,JUE,VIE)\n";
+         	for(int)//ingresar la condicion de LUN=LUNES, etc.
+
          cin<<dia ;
          cout>>"ingrese hora de incio de la clase\n";
          cin<<hora_1;
          cout>>"ingrese la hora del termino de clase\n";
          cin<<hora_2;
+cout>>"hora ingrasada: \n";
+    cout>>dia": " hora_1" - "hora_2"\n";
+    cout>>semestre"\n";
+    cout>>materia;
 
- getch();
+
+
+
+//F2= SEGUNDO BOTON - VER HOARIO DE HOY
+
+//F3= TERCER BOTON - VER HORARIO DE LA SEMANA
+
+//F4= CUARTO BOTON - SALIR
+
+
+
+main()
+
+{
+
+getch();
 }
+
+
