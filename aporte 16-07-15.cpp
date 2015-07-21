0b7ea19d177b927c0325c4bf5080565f9610722b
LuisDM <luisdav.crack@hotmail.com>
2015-07-20 21:39:08 -0500

    aporte 20/07/2015
    aporte  de la condición para el ingreso de la variable DIA

--- aporte 16-07-15.cpp
+++ aporte 16-07-15.cpp
@@ -4,18 +4,61 @@
 #include<stdio>
 
 
-//F1= PRIMER BOTON - INGRESAAR HOA DE CLASE
+//F1= PRIMER BOTON - INGRESAAR HORA DE CLASE
 int hora_1,hora_2,materia,semestre;//condicion de obligatorio
 char dia [3];
-
+char diacompleto [10];
+bool ban;
          cout>>"ingrese nombre de materia\n";
          cin<<materia;
          cout>>"ingrese el semestre\n";
          cin<<semestre;
          cout>>"ingres el dia(LUN,MART,MIE,JUE,VIE)\n";
-         	for(int)//ingresar la condicion de LUN=LUNES, etc.
+         	//ingresar la condicion de LUN=LUNES, etc.
+            cin<<dia ;
+            switch(dia);
+            do
+            	if (stramp(dia, "LUN")==0)
+               streepy(diacompleto, "Lunes");
+               {
+               	ban = 1;
+               	ban = true;
+               }
 
-         cin<<dia ;
+               else if (stramp(dia, "MAR")==0)
+               streepy(diacompleto, "Martes");
+               {
+                	ban = 1;
+               	ban = true;
+               }
+
+               else if (stramp(dia, "MER")==0)
+               streepy(diacompleto, "Mercoles");
+               {
+               	ban = 1;
+               	ban = true;
+
+               else if (stramp(dia, "JUE")==0)
+               streepy(diacompleto, "Jueves");
+               {
+               	ban = 1;
+               	ban = true;
+               }
+
+               else (stramp(dia, "VIE")==0)
+               streepy(diacompleto, "Viernes");
+               {
+               	ban = 1;
+               	ban = true;
+               }
+
+               	else cout<<"valor no definido, vuelva a ingresar. "; ban = 0;
+            while(ban == 0);
+
+
+            getch();
+            }
+
          cout>>"ingrese hora de incio de la clase\n";
          cin<<hora_1;
          cout>>"ingrese la hora del termino de clase\n";
@@ -24,7 +67,6 @@ cout>>"hora ingrasada: \n";
     cout>>dia": " hora_1" - "hora_2"\n";
     cout>>semestre"\n";
     cout>>materia;
-
 
 
 
